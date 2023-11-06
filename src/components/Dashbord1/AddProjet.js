6359e657abd16046325ebd5d38b046ac7c61595b
kamdaeva <malvina.kamd@facsciences-uy1.cm>
2023-11-06 08:31:48 +0100

    dashborsd1

--- src/components/Dashbord1/AddProjet.js
+++ src/components/Dashbord1/AddProjet.js
@@ -3,22 +3,219 @@ import Box from '@mui/material/Box';
 import Sidebar from './Sidebar.js'
 import NavBar from './NavBar.js'
 import Container from 'react-bootstrap/Container';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import InputAdornment from '@mui/material/InputAdornment';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
 
 
 const AddProjets = () => {
+  const [Domaine, setDomaine] = React.useState('');
+
+  const handleChange = (event) => {
+    setDomaine(event.target.value);
+  };
+
   return ( 
 
     <>
       <NavBar/>
-      <Container>
-      <Box className='mt-5' sx={{ display: 'flex' }}>
+     
+      <Box className='mt-5' sx={{ display: 'flex' ,flexDirection:'column'}}>
         <Sidebar/>
         <Box sx={{ p: 3 }}>
-          <h1>AddProjets</h1>
+          <Typography fontWeight="bold" noWrap className='fs-3 text-center'>
+            Ajouter Un Projtes
+          </Typography>
         </Box>
+        <Grid className='mt-5 mt-sm-0'>
+          <Grid>
+          <Container className='mt-5'>
+            <div className="row ">
+              <div className='col'>
+                <FormControl fullWidth sx={{ m: 1,}} variant="outlined">
+                  <FormHelperText id="outlined-projet-helper-text">Nom du Projet</FormHelperText>
+                  <OutlinedInput
+                    id="outlined-adornment-projet"
+                    // endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                    aria-describedby="outlined-projet-helper-text"
+                    inputProps={{
+                      'aria-label': 'projet',
+                    }}
+                  />
+                </FormControl>
+              </div>
+            </div>
+            <div className="row justify-content-between">
+              <div class="col-8">
+                <FormControl  fullWidth sx={{ m: 1,}} variant="outlined">
+                  <FormHelperText id="outlined-projet-helper-text">Tags</FormHelperText>
+                  <OutlinedInput
+                    id="outlined-adornment-projet"
+                    // endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                    aria-describedby="outlined-projet-helper-text"
+                    inputProps={{
+                      'aria-label': 'projet',
+                    }}
+                  />
+                </FormControl>
+              </div>
+              <div class="col-4">
+                <FormControl fullWidth  sx={{ m: 1}}>
+                  <FormHelperText id="outlined-projet-helper-text">Domaines</FormHelperText>
+                  <InputLabel id="demo-select-small-label"></InputLabel>
+                  <Select
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={Domaine}
+                    // label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              
+              <div className='row'>
+                <div className='row'>
+                  <div className='col'>
+                    <Typography variant="h5" color="text.dark" fontWeight="semiBold" className='fs-5 mt-2 p-2'> 
+                      Salaires
+                    </Typography>
+                  </div>
+               </div>
+                <div className='col-4'>
+                  <FormControl  fullWidth sx={{ m: 1,}} variant="outlined">
+                    <FormHelperText id="outlined-projet-helper-text">Minimum</FormHelperText>
+                    <OutlinedInput
+                      id="outlined-adornment-projet"
+                      endAdornment={<InputAdornment position="end">XAF</InputAdornment>}
+                      aria-describedby="outlined-projet-helper-text"
+                      inputProps={{
+                        'aria-label': 'projet',
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <div className='col-4'>
+                  <FormControl  fullWidth sx={{ m: 1,}} variant="outlined">
+                    <FormHelperText id="outlined-projet-helper-text">Maximum</FormHelperText>
+                    <OutlinedInput
+                      id="outlined-adornment-projet"
+                      endAdornment={<InputAdornment position="end">XAF</InputAdornment>}
+                      aria-describedby="outlined-projet-helper-text"
+                      inputProps={{
+                        'aria-label': 'projet',
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <div class="col-4">
+                <FormControl fullWidth  sx={{ m: 1}}>
+                  <FormHelperText id="outlined-projet-helper-text">Devises</FormHelperText>
+                  <InputLabel id="demo-select-label"></InputLabel>
+                  <Select
+                    labelId="demo-select-label"
+                    id="demo-select-small"
+                    value={Domaine}
+                    // label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              </div>
+              <div className='row'>
+                <div className='row'>
+                  <div className='col'>
+                    <Typography variant="h5" color="text.dark" fontWeight="semiBold" className='fs-5 mt-2 p-2'> 
+                      Informations
+                    </Typography>
+                  </div>
+               </div>
+                <div className='col-4'>
+                  <FormControl  fullWidth sx={{ m: 1,}} variant="outlined">
+                    <FormHelperText id="outlined-projet-helper-text">Date De creation</FormHelperText>
+                    <OutlinedInput
+                      id="outlined-adornment-projet"
+                      endAdornment={<InputAdornment position="end">< CalendarMonthIcon/></InputAdornment>}
+                      aria-describedby="outlined-projet-helper-text"
+                      inputProps={{
+                        'aria-label': 'projet',
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <div className='col-4'>
+                  <FormControl  fullWidth sx={{ m: 1,}} variant="outlined">
+                    <FormHelperText id="outlined-projet-helper-text">Date D’expiration</FormHelperText>
+                    <OutlinedInput
+                      id="outlined-adornment-projet"
+                      endAdornment={<InputAdornment position="end">< CalendarMonthIcon/></InputAdornment>}
+                      aria-describedby="outlined-projet-helper-text"
+                      inputProps={{
+                        'aria-label': 'projet',
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <div class="col-4">
+                <FormControl fullWidth  sx={{ m: 1}}>
+                  <FormHelperText id="outlined-projet-helper-text">Modules</FormHelperText>
+                  <InputLabel id="demo-select-label"></InputLabel>
+                  <Select
+                    labelId="demo-select-label"
+                    id="demo-select-small"
+                    value={Domaine}
+                    // label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              </div>
+            </div>
+            <Container>
+              <div className='row'> 
+                 <div className='row'>
+                  <div className='col'>
+                    <Typography variant="h5" color="text.dark" fontWeight="semiBold" className='fs-5 mt-2 p-2'> 
+                      localisation
+                    </Typography>
+                  </div>
+               </div>
+              </div>
+            </Container>
+          </Container>
+          </Grid>
+        </Grid>
+      
        
       </Box>
-      </Container>
     </>
    
   
--- src/components/Dashbord1/Projet.js
+++ src/components/Dashbord1/Projet.js
@@ -2,8 +2,55 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Sidebar from './Sidebar.js'
 import NavBar from './NavBar.js'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Container from 'react-bootstrap/Container';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
 
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.common.black,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 const Projets = () => {
   return ( 
@@ -13,10 +60,93 @@ const Projets = () => {
       <Box className='mt-5' sx={{ display: 'flex' }}>
         <Sidebar/>
         <Box sx={{ p: 3 }}>
-          <h1>Projets</h1>
+          {/* <h1>Projets</h1> */}
         </Box>
-
       </Box>
+        <Box>
+          <div class="row justify-content-around">
+            <div class="col-4">
+             <Typography fontWeight="bold" noWrap className='fs-3 text-center'>
+                Mes projets
+              </Typography>
+            </div>
+            {/* <div class="col-4">
+             <Typography fontWeight="bold" noWrap className='fs-5 text-primary'>
+                Tout voir
+              </Typography>
+            </div> */}
+          </div>
+        </Box>
+     
+       <Container className='mt-3'>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Emplois</StyledTableCell>
+                  <StyledTableCell align="right">Statut</StyledTableCell>
+                  <StyledTableCell align="right">Freelance</StyledTableCell>
+                  <StyledTableCell align="right">Action</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+              
+                  <StyledTableRow>
+                    <StyledTableCell component="th" scope="row">
+                      Mode
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      <Button  color="error" size="small">
+                        expiré
+                      </Button>
+                    </StyledTableCell>
+                    <StyledTableCell align="right">2</StyledTableCell>
+                    <StyledTableCell align="right">
+                    <Stack direction="row" spacing={1} className='d-flex justify-content-end'>
+                        <IconButton aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                        <IconButton  aria-label="">
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton  aria-label="">
+                          <VisibilityIcon />
+                        </IconButton>
+                      </Stack>
+                    </StyledTableCell>
+                  </StyledTableRow>
+
+                  <StyledTableRow>
+                    <StyledTableCell component="th" scope="row">
+                      Mode
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      <Button  color="success" size="small">
+                        activé
+                      </Button>
+                    </StyledTableCell>
+                    <StyledTableCell align="right">2</StyledTableCell>
+                    <StyledTableCell align="right">
+                    <Stack direction="row" spacing={1} className='d-flex justify-content-end'>
+                        <IconButton aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                        <IconButton  aria-label="">
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton  aria-label="">
+                          <VisibilityIcon />
+                        </IconButton>
+                      </Stack>
+                    </StyledTableCell>
+                  </StyledTableRow>
+              
+              </TableBody>
+            </Table>
+          </TableContainer>
+      </Container>
+
+  
       
     </>
    
